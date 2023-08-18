f6b8dbcaae09b7901a5af55558fe6cf5c10682f9
AlexanderLepilov <lepilow.alexander@mail.ru>
2023-08-18 14:49:22 +0300

    Dz 1

---
+++ Home_work_1/InMemoryModel/ModelStore.java
@@ -0,0 +1,47 @@
+package Home_work_1.InMemoryModel;
+
+import java.util.ArrayList;
+import java.util.List;
+import Home_work_1.ModelElements.Camera;
+import Home_work_1.ModelElements.Flash;
+import Home_work_1.ModelElements.PoligonalModel;
+import Home_work_1.ModelElements.Scene;
+
+public class ModelStore implements IModelChanger {
+
+    public List<PoligonalModel> Models;
+    public List<Scene> Scenes;
+    public List<Flash> Flashes;
+    public List<Camera> Cameras;
+    private List<IModelChangerObserver> changerObservers;
+
+    public ModelStore(List<IModelChangerObserver> changerObservers, List<PoligonalModel> models, List<Scene> scene, List<Flash> Flashes, List<Camera> Cameras) throws Exception {
+
+        this.changerObservers = changerObservers;
+        Models = new ArrayList<>(null);
+        Scenes = new ArrayList<>(null);
+        Flashes = new ArrayList<>(null) ;
+        Cameras = new ArrayList<>(null);
+
+        Models.add(new PoligonalModel(null));
+        Flashes.add(new Flash());
+        Cameras.add(new Camera());
+        Scenes.add(new Scene(0, Models, Flashes, Cameras));
+
+        }
+
+    public void NotifyChange(IModelChanger sender) {
+    }
+
+    public Scene getScena(int id) {
+        for (int i = 0; i < Scenes.size(); i++) {
+
+            if(Scenes.get(i).Id == id) {
+                return Scenes.get(i);
+            }
+        }
+        return null;
+
+    }
+
+}
---
+++ Home_work_1/Main.java
@@ -0,0 +1,8 @@
+package Home_work_1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+
+}

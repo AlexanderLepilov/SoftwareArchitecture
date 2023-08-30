ea71fff6b97968a20443a092353834f54bece406
AlexanderLepilov <lepilow.alexander@mail.ru>
2023-08-30 21:13:19 +0300

    Home Work 3

---
+++ Home_work_3/Car/TypeFuel.java
@@ -0,0 +1,8 @@
+package Home_work_3.Car;
+
+public enum TypeFuel {
+
+    Gasoline,
+    Diesel
+
+}
---
+++ Home_work_3/Car/Wiping.java
@@ -0,0 +1,11 @@
+package Home_work_3.Car;
+
+public interface Wiping {
+
+    public void wipWindshield();
+
+    public void wipHeadlights();
+
+    public void wipMirrors();
+
+}
